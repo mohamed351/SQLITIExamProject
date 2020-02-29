01633e88a85c4a75d8fc45a3a14ddb72fbb94b20
Kareem Sherrif <k.sherrif95@gmail.com>
2020-02-29 13:22:01 +0200

    Update

--- Mohamed Beshri.sql
+++ Mohamed Beshri.sql
@@ -1,14 +1,14 @@
 -- ISCorrect not Correct Answer
 --Types 
--- used this type in Stored Procdure sp_Add_Question_Choice
-Create type ty_Choocies as table
+-- used this type in Stored Procdure SP_Add_Question_Choice
+Create type TY_Choices as table
 (
  [ChoiceText] nvarchar(max),
  [IsCorrect] bit 
 )
 GO
 --used this type in Stored Procdure SP_ExamCorrection
-CREATE TYPE [dbo].[ty_Answer_Question] AS TABLE(
+CREATE TYPE [dbo].[TY_Question_Answer] AS TABLE(
 	[QuestionID] [int] NULL,
 	[AnswerID] [int] NULL,
 	[QuestionText] [nvarchar](max) NULL
@@ -16,11 +16,11 @@ CREATE TYPE [dbo].[ty_Answer_Question] AS TABLE(
 -- end types
 GO
 --Used to Add Question With their choices
-Create proc sp_Add_Question_Choice
+Create proc SP_Add_Question_Choice
 @text nvarchar(max),
 @typeOfQuestion nvarchar(20),
 @questionScore int,
-@tableChoocies ty_Choocies readonly 
+@tableChoocies TY_Choices readonly 
 as
 begin try
 begin transaction 
@@ -35,11 +35,11 @@ begin catch
  rollback transaction
 end catch
 GO
---this stored Correct Exam
+-- Exam Answers & Correction
 CREATE proc [dbo].[SP_ExamCorrection]
 @ExamID int , 
 @StudentID int,
-@tbl_AnswerIt ty_Answer_Question readonly
+@tbl_AnswerIt TY_Question_Answer readonly
 as
 begin try
 begin transaction
@@ -59,7 +59,7 @@ begin catch
 rollback transaction
 end catch
 GO
--- Show Correct Answer
+-- Show Correct Answer -- Model Answer
 Create proc SP_Select_ExamQuestions
 @examID int
 as
@@ -71,7 +71,7 @@ on cho.QuestionID = quest.ID
 where ex.ID =@examID and cho.IsCorrect =1
 GO
 --Show Just Grade without persent
-Create proc [dbo].[sp_Select_Student_ExamReport]
+Create proc [dbo].[SP_Select_Student_ExamReport]
 @studentID int 
 as
 Select student.ID, FName,exm.Name as [Subject], sum(Grade) as Grade from Student as student inner join StudentAnswers as answer
@@ -82,7 +82,7 @@ group by  student.ID, FName,exm.Name
 GO
 -- stored for Reports
 -- Get Students In Track Report
-CREATE proc [dbo].[sp_Select_Student_Report]
+CREATE proc [dbo].[SP_Select_Student_Report]
 @TrackID int 
 as
 Select distinct FName,LName ,trk.Name from Student as st 
@@ -92,7 +92,7 @@ where TrackID=@TrackID
 GO
 
 --report Insructors
-CREATE proc [dbo].[sp_Select_InsructorReport]
+CREATE proc [dbo].[SP_Select_InsructorReport]
 @InstructorID int 
 as
 Select ins.Name as InsructorName  ,ISNULL(course.Name,'No Course Teach') as CourseName , count(studentcourse.StudentID) as ANumberOfStudent from Instructor as ins 
@@ -104,7 +104,7 @@ where ins.ID = @InstructorID
 group by ins.Name ,course.ID ,course.Name
 GO
 --report ExamReport
-Create proc [dbo].[sp_Select_Student_ExamReport]
+Create proc [dbo].[SP_Select_Student_ExamReport]
 @studentID int 
 as
 Select student.ID, FName,exm.Name as [Subject], sum(Grade) as Grade from Student as student inner join StudentAnswers as answer
